5e9a22e6dfbba9de50eaac3b4ab6e80f765b8db8
Shannon Young <shannon.ari1@gmail.com>
2021-05-31 11:59:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,18 @@
 class Song
   attr_accessor :artist, :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def artist_name
-    if artist
-      self.artist.name
-    else
-      nil
-    end
+    self.artist
   end
-
 end
